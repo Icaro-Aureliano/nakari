6a3d2c9222b107a285ed84f291f4316f02068f8d
Icaro <aurelianoicaro@gmail.com>
2024-06-15 11:19:11 -0300

    Add do index.html

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from app import create_app
-from flask import Flask, render_template_string
-
+from flask import render_template
 app = create_app()
 
 @app.route('/')
-def list_routes():
-    output = []
-    for rule in app.url_map.iter_rules():
-        if f"{rule}"[-1] == "/" and f"{rule}" not in output and f"{rule}" != "/static/" and f"{rule}" != "/":     
-            line = f"<a href='{rule}'>{rule}</a>"
-            output.append(line)
-    
-    return render_template_string('<pre>' + '\n'.join(output) + '</pre>')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
